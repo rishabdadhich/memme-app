f875d7874e0ac50e770a906604bb329b0c66d371
Rishabh <rishabh@Abhinavs-Mac.local>
2017-07-15 14:18:34 +0530

    Initial Commit

---
+++ memme-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memme-app
+//
+//  Created by Rishabh on 24/04/1939 Saka.
+//  Copyright © 1939 Saka rishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
